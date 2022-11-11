17bf613d4098afa34778cc0f51f9a3988f90b9f5
sseen <ssnfee@gmail.com>
2022-11-11 11:30:18 +0900

    add menu item and toast

--- app/src/main/java/com/dinkar/blescanner/ui/wifi/ListWifiActivity.kt
+++ app/src/main/java/com/dinkar/blescanner/ui/wifi/ListWifiActivity.kt
@@ -2,9 +2,14 @@ package com.dinkar.blescanner.ui.wifi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.dinkar.blescanner.BaseDetailActivity
 import com.dinkar.blescanner.R
 import com.dinkar.blescanner.ui.wifi.ui.wifi.ListWifiFragment
+import es.dmoral.toasty.Toasty
 
 class ListWifiActivity : BaseDetailActivity() {
 
@@ -17,5 +22,20 @@ class ListWifiActivity : BaseDetailActivity() {
                 .replace(R.id.container, ListWifiFragment.newInstance())
                 .commitNow()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.save_item) {
+            Toasty.success(applicationContext, "Success!", Toast.LENGTH_SHORT, true).show();
+
+            finish()
+        }
+        return true
     }
 }
