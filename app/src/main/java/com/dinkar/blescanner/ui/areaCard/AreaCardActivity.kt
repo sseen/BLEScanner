da83054122f962a2248bfdfd7ed94aa49b1e0bda
sseen <ssnfee@gmail.com>
2022-11-11 14:01:07 +0900

    练习下list card view

---
+++ app/src/main/java/com/dinkar/blescanner/ui/areaCard/AreaCardActivity.kt
@@ -0,0 +1,12 @@
+package com.dinkar.blescanner.ui.areaCard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dinkar.blescanner.R
+
+class AreaCardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_area_card)
+    }
+}
