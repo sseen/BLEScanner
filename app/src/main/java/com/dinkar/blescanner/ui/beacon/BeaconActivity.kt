836556356cd4bec34ec481b133f8ee70134e370f
sseen <ssnfee@gmail.com>
2022-11-11 09:55:51 +0900

    add back menu bt

--- app/src/main/java/com/dinkar/blescanner/ui/beacon/BeaconActivity.kt
+++ app/src/main/java/com/dinkar/blescanner/ui/beacon/BeaconActivity.kt
@@ -2,11 +2,27 @@ package com.dinkar.blescanner.ui.beacon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.dinkar.blescanner.R
 
 class BeaconActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_beacon)
+
+        var actionBar = supportActionBar
+        actionBar?.let {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
